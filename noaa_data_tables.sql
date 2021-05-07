4d5f5d99b78d5dd3f40405fb1511809436cb5737
darrida <darrida.py@gmail.com>
2021-05-07 17:18:48 -0500

    added information about geojson for countries, indexes, and index cleanup into insert script

--- noaa_data_tables.sql
+++ noaa_data_tables.sql
@@ -24,3 +24,31 @@ CREATE TABLE IF NOT EXISTS climate.noaa_year_averages (
 ) USING HEAP;
 
 SELECT AddGeometryColumn('','climate.noaa_year_averages','geom','4326','POINT', 2);
+
+create index noaa_year_averages_year_idx on climate.noaa_year_averages (year);
+
+CREATE INDEX noaa_year_averages_geom_idx
+  ON climate.noaa_year_averages
+  USING GIST (geom);
+
+ANALYZE climate.noaa_year_averages;
+VACUUM ANALYZE climate.noaa_year_averages;
+
+create index countries_name_idx on climate.countries (name);
+
+CREATE INDEX countries_geom_idx
+  ON climate.countries
+  USING GIST (wkb_geometry);
+
+ANALYZE climate.countries;
+VACUUM ANALYZE climate.countries;
+
+select * 
+from climate.noaa_year_averages t1
+join climate.countries c1
+on st_contains(c1.wkb_geometry, t1.geom)
+where c1.geounit = 'United States of America';
+
+select *
+from climate.countries c1
+where c1.name = 'United States';
