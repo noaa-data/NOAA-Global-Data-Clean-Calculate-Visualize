7c35cabe5ab6408265d8533cffb6d22c85c70527
darrida <darrida.py@gmail.com>
2021-05-12 02:49:27 -0500

    save

--- prefect_aws_data_prep.py
+++ prefect_aws_data_prep.py
@@ -345,7 +345,7 @@ def calculate_year_csv(year_folder, bucket_name, region_name, wait_for: str):
 
 
 # IF REGISTERING FOR THE CLOUD, CREATE A LOCAL ENVIRONMENT VARIALBE FOR 'EXECTOR' BEFORE REGISTERING
-coiled_ex = False
+coiled_ex = True
 if coiled_ex == True:
     print("Coiled")
     coiled.create_software_environment(
